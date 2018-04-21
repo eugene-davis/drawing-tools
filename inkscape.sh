32d2119d1fcd61a35402e2a8e5c97519520d707a
Eugene Davis <eugene@industrialdragonfly.com>
2018-04-21 13:34:33 +0200

    Add gimp install and move scripts into directory

---
+++ inkscape.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Call from the images folder with the name of the image, and it'll open the image directly,
+# otherwise it might get angry at you.
+
+name=$1
+
+open -a XQuartz
+IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+xhost + $ip
+
+docker run --rm -e DISPLAY=$IP:0 -v $(pwd)/$name/fonts/:/root/.local/share/fonts/TTF/type1 -v $(pwd)/$name:/image -v /tmp/.X11-unix:/tmp/.X11-unix drawing-tools /usr/bin/inkscape /image/$name.svg
