099d6c231c3e51ec3af26e6da965f81441b16f59
Eugene Davis <eugene@industrialdragonfly.com>
2018-04-21 14:25:13 +0200

    Update naming convention for gimp

--- gimp.sh
+++ gimp.sh
@@ -9,4 +9,4 @@ open -a XQuartz
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $ip
 
-docker run --rm -e DISPLAY=$IP:0 -v $(pwd)/$name/fonts/:/root/.local/share/fonts/TTF/type1 -v $(pwd)/$name:/image -v /tmp/.X11-unix:/tmp/.X11-unix drawing-tools /usr/bin/gimp /image/sketch.xcf
+docker run --rm -e DISPLAY=$IP:0 -v $(pwd)/$name/fonts/:/root/.local/share/fonts/TTF/type1 -v $(pwd)/$name:/image -v /tmp/.X11-unix:/tmp/.X11-unix drawing-tools /usr/bin/gimp /image/name.xcf
