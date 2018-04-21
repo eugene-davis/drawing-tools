4c6209ef3a534f40969a4aed4b4cc1fa25f027bf
Eugene Davis <eugene@industrialdragonfly.com>
2018-04-21 15:18:06 +0200

    Updated launch scripts

--- inkscape.sh
+++ inkscape.sh
@@ -5,8 +5,10 @@
 
 name=$1
 
+docker pull nobodyfromaroundhere/drawing-tools:latest
+
 open -a XQuartz
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $ip
 
-docker run --rm -e DISPLAY=$IP:0 -v $(pwd)/$name/fonts/:/root/.local/share/fonts/TTF/type1 -v $(pwd)/$name:/image -v /tmp/.X11-unix:/tmp/.X11-unix drawing-tools /usr/bin/inkscape /image/$name.svg
+docker run --rm -e DISPLAY=$IP:0 -v $(pwd)/$name/fonts/:/root/.local/share/fonts/TTF/type1 -v $(pwd)/$name:/image -v /tmp/.X11-unix:/tmp/.X11-unix nobodyfromaroundhere/drawing-tools /usr/bin/inkscape /image/$name.svg
